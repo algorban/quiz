43198d8b330a59f9a2c7acb1f8f9c6d82eaa39df
Oleksandr Gorban <oleksandr_gorban@epam.com>
2016-01-14 21:57:31 -0500

    Initial commit

---
+++ src/main/java/StockPrice.java
@@ -0,0 +1,26 @@
+package main.java;
+
+/**
+ * Created by root on 1/14/2016.
+ */
+public class StockPrice {
+
+    public static void main(String[] args) {
+        int[] stockPricesYesterday = new int[]{10, 7, 5, 8, 11, 9};
+        System.out.println(getMaxProfit(stockPricesYesterday));
+    }
+
+    public static int getMaxProfit(int[] stocks) {
+        if(stocks.length<2) throw new IllegalArgumentException("Minimum 2 elements in array required");
+
+        int profit = stocks[1] - stocks[0];
+        int min = stocks[0];
+        for(int i=1; i< stocks.length; i++) {
+            int potential_profit = stocks[i] - min;
+            profit = Math.max(potential_profit, profit);
+            min = Math.min(stocks[i], min);
+        }
+
+        return profit;
+    }
+}
